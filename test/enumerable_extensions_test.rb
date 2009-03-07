0c992208ef2de14a55f3582062d4d03883e08a7e
Marcos Arias <markus@agil-e.com>
2009-03-07 17:36:58 +0100

    Added tests to Array and Enumerable. Added a console for rapid prototyping new functions and testing functionality. Removed unused test_case

--- test/enumerable_extensions_test.rb
+++ test/enumerable_extensions_test.rb
@@ -1,5 +1,39 @@
 require 'test_helper'
 
-class EnumerableExtensionsTest < ActiveSupport::TestCase
+class EnumerableNotEmptyTest < ActiveSupport::TestCase
+  
+  test "responds to not_empty?" do
+    assert_respond_to({}, :not_empty?)
+    assert_respond_to([], :not_empty?)
+  end
+  
+  test "has values then returns true" do
+    assert({:key => 'value'}.not_empty?)
+    assert(['value'].not_empty?)
+  end
+  
+  test "is empty then return false" do
+    assert_false({}.not_empty?)
+    assert_false([].not_empty?)
+  end
+  
+end
+
+class EnumerableNotIncludeTest < ActiveSupport::TestCase
+  
+  test "responds to not_include?" do
+    assert_respond_to({}, :not_include?)
+    assert_respond_to([], :not_include?)
+  end
+  
+  test "value is not present then returns true" do
+    assert({:subject => 'a person'}.not_include?(:verb))
+    assert(['a person'].not_include?('sleeps'))
+  end
+  
+  test "value is present then returns false" do
+    assert_false({:subject => 'a person', :verb => 'sleep'}.not_include?(:verb))
+    assert_false(['a person', 'sleeps'].not_include?('sleeps'))
+  end
   
 end
