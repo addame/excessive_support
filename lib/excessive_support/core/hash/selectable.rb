8b1e4c911bd2a5d9dd242c74b3b521c3efac0e12
Marcos Arias <markus@agil-e.com>
2009-03-09 12:28:10 +0100

    Removed duplicated methdos

--- lib/excessive_support/core/hash/selectable.rb
+++ lib/excessive_support/core/hash/selectable.rb
@@ -3,23 +3,30 @@ module ExcessiveSupport #:nodoc:
     module Hash #:nodoc:
       # Various methods to select certain values from a hash
       #
-      # TODO: make some elements destructive
       # TODO: implement ability to pass a block as argument
       module Selectable
 
         # The hash without certain values
-        def except(*exclusions)
-          self.reject { |key, value| exclusions.include? key.to_sym }.to_hash
-        end
+        # !! Already exists in ActiveSupport
+        # def except(*exclusions)
+        #   self.reject { |key, value| exclusions.include? key.to_sym }.to_hash
+        # end
         
         # The hash with only certain values
-        def only(*selections)
-          self.select { |key, value| selections.include? key.to_sym }.to_hash
-        end
-
+        # !! Already exists in ActiveSupport (named slice)
+        # def only(*selections)
+        #   self.select { |key, value| selections.include? key.to_sym }.to_hash
+        # end
+        alias_method :only, :slice
+        alias_method :only!, :slice!
+        
         # The hash with certain values overrided
         def with(overrides = {})
           self.merge overrides
+        end
+        
+        def with!(overrides = {})
+          self.merge! overrides
         end
         
       end
